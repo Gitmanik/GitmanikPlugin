6bd7bf7fa36616c2487348f3f8258e022d1ea186
Paweł Reich <thegitman@outlook.com>
2020-10-26 12:21:23 +0100

    Dziala.

--- src/main/java/pl/gitmanik/events/MruwiKilofCraftingHandler.java
+++ src/main/java/pl/gitmanik/events/MruwiKilofCraftingHandler.java
@@ -1,8 +1,9 @@
 package pl.gitmanik.events;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 import pl.gitmanik.GitmanikPlugin;
@@ -10,14 +11,13 @@ import pl.gitmanik.GitmanikPlugin;
 public class MruwiKilofCraftingHandler implements Listener {
 
     @EventHandler
-    public void onMruwiKilofCraft(CraftItemEvent event){
+    public void onMruwiKilofCraft(PrepareItemCraftEvent event){
         CraftingInventory inv = event.getInventory();
 
-        if(inv.getResult().isSimilar(GitmanikPlugin.mruwiKilof)){
+        if(inv.getResult() != null && inv.getResult().equals(GitmanikPlugin.mruwiKilof)){
             for(ItemStack item : inv.getMatrix()){
-                if (item != null && !item.isSimilar(GitmanikPlugin.mruwiKlejnot)){
+                if (item != null && item.getType() == Material.LAPIS_BLOCK && !item.equals(GitmanikPlugin.mruwiKlejnot)){
                     inv.setResult(null);
-                    event.setCancelled(true);
                 }
             }
         }
